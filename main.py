24893b8bfba9f47ffac50c1ccdeafa10998dcb18
Kadu Melo <kaduchessi@gmail.com>
2024-04-22 19:56:55 -0300

    Added logic to generate videos using HeyGen

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pandas as pd
 from elevenlabs_audio import generate_mp3
+import os
 
 df = pd.read_csv('textos.csv')
 
+if not os.path.exists('audios'):
+   os.makedirs('audios')
+
 for index, row in df.iterrows():
     print(row['Texto'])
-    generate_mp3(row['Texto'], "teste.mp3")
+    generate_mp3(row['Texto'], "audios/teste.wav")
