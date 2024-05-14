c67148b8986dfbcb9fba0e2c4ce1e7f84e8e5fb4
Kadu Melo <kaduchessi@gmail.com>
2024-05-14 14:57:04 -0300

    Added logic to generate audios with dynamic names

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import pandas as pd
+import unidecode
 from elevenlabs_audio import generate_mp3
 import os
 
-df = pd.read_csv('textos.csv')
+path_file = os.getenv("PATH_FILE")
+df = pd.read_excel(path_file, usecols='B')
 
 if not os.path.exists('audios'):
    os.makedirs('audios')
 
-for index, row in df.iterrows():
-    print(row['Texto'])
-    generate_mp3(row['Texto'], "audios/teste.wav")
+for text in df['texto'].values.tolist():
+   name = unidecode.unidecode(text.split(',')[0].lower())
+   file_name = "{}/{}.{}".format("audios", name, "wav")
+   print(file_name)
+   generate_mp3(text, file_name)
